811c5518d7f8ebee744608a438014bbbb70d168c
TonGarcia <ton_unb@hotmail.com>
2014-01-31 15:26:48 -0200

    Projeto base em Rails (commit inicial)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProumoBox::Application.config.secret_key_base = '9915202f2b4f43f408941f64f8604ae4d8643561dc0d7cd2ec491bd74ea6aac5cfac6eb1ee5760e45ecf45d6ae9fb7d267f7fcded74875cb16b0cf0ff99eb01d'
